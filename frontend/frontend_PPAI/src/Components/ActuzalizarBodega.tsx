88a510f09accbf160ef87eed74b01824b4b933c8
tapari99502 <99502@sistemas.frc.utn.edu.ar>
2024-11-11 16:42:16 -0300

    Ahora tenemos Bootstrap

--- frontend/frontend_PPAI/src/Components/ActuzalizarBodega.tsx
+++ frontend/frontend_PPAI/src/Components/ActuzalizarBodega.tsx
@@ -1,6 +1,7 @@
 export default function ActualizarBodegas(){
     return (
         <div>
+            <h1>Esto es Bodegas</h1>
         </div>
     )
 }
--- frontend/frontend_PPAI/src/Components/PaginaInicio.tsx
+++ frontend/frontend_PPAI/src/Components/PaginaInicio.tsx
@@ -1,6 +1,7 @@
 export default function PaginaInicio() {
     return (
         <div>
+            <h2>Esto es inicio</h2>
         </div>
     )
 }
