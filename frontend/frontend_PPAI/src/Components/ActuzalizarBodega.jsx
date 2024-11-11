52cb3c177a038c68f3a24cd90194fbbeafedd282
tapari99502 <99502@sistemas.frc.utn.edu.ar>
2024-11-11 16:27:59 -0300

    Me cambio a la compu

---
+++ frontend/frontend_PPAI/src/Components/ActuzalizarBodega.jsx
@@ -0,0 +1,6 @@
+export default function ActualizarBodegas(){
+    return (
+        <div>
+        </div>
+    )
+}
--- frontend/frontend_PPAI/src/main.tsx
+++ frontend/frontend_PPAI/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Route, Routes } from 'react-router-dom'
+import PaginaInicio from './components/PaginaInicio'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Routes>
+      <Route path="/" element={<PaginaInicio />} />
+    </Routes>
     <App />
   </StrictMode>,
 )
