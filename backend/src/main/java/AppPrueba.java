aa9fba17a5d7388f67abb8eeca421494e0e1f299
Brenda Tapari <brendatapa6@gmail.com>
2024-11-11 21:45:41 -0300

    Cambios

--- backend/src/main/java/AppPrueba.java
+++ backend/src/main/java/AppPrueba.java
@@ -20,6 +20,21 @@ public class AppPrueba {
         List<Reseña> Reseñas = new ArrayList<>();
         Reseñas.add(reseña);
         Vino vino = new Vino("soy un Vino!", bodega, 2, LocalDateTime.now(), "Imagen de vino", "Soy una nota", 2, Maridajes, Varietales, Reseñas);
+        List<Vino>Vinos = new ArrayList<>();
+        Vinos.add(vino);
+        Vino vino2 = new Vino("soy un Vino2!", bodega, 2, LocalDateTime.now(), "Imagen de vino", "Soy una nota2", 2, Maridajes, Varietales, Reseñas);
+        Vinos.add(vino2);
+
+
+
+        System.out.println(
+        uva.mostrarNombre()
+        );
+        System.out.println( reseña.getFECHA_RESEÑA());
+        System.out.println(reseña.esPremium());
+        System.out.println(vino.calcularRanking());
+        System.out.println(vino.sosVinoAActualizar(Vinos));
+        System.out.println(Vinos);
 
     }
 }
--- backend/src/main/java/com/PPAIEnJava/NuevoPPAI/Controlers/GestorController.java
+++ backend/src/main/java/com/PPAIEnJava/NuevoPPAI/Controlers/GestorController.java
@@ -5,10 +5,7 @@ import com.PPAIEnJava.NuevoPPAI.Models.NoPersistent.VinoActualizado;
 import com.PPAIEnJava.NuevoPPAI.Models.Persistent.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -16,6 +13,8 @@ import java.util.List;
 
 @RestController()
 @RequestMapping(path = "/gestor")
+@ResponseBody
+@CrossOrigin
 public class GestorController {
 
     private final Gestor gestor;
@@ -43,7 +42,7 @@ public class GestorController {
 
 
     @GetMapping(path = "/resultadoTablas")
-    public List<VinoActualizado> resultadoTablas(@PathVariable String bodegaSeleccionada){
+    public List<VinoActualizado> resultadoTablas(@RequestParam String bodegaSeleccionada){
         List<VinoActualizado> vinoActualizados = new ArrayList<>();
         Bodega bodega = new Bodega("Historia", "Nombre", "Descripcion", 4, LocalDateTime.now(), 34);
         TipoUva uva = new TipoUva("Soy una uva", "Soy una descripcion");
